8b1042aaaad0e4f6b02a778f50530f6e6cd8fb17
arthur fuchs <alfuchs17@gmail.com>
2018-09-30 10:54:17 -0300

    pause menu
    added the possibility to pause the game

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,8 +2,28 @@ extends Node
 
 func _ready():
 	$player.connect("killed", self, "restart_scene")
-		
+	
 
 func restart_scene():
 	get_tree().reload_current_scene()
 	
+func pause():
+	get_tree().paused = true;
+	
+func resume():
+	get_tree().paused = false;
+
+func _on_pause_button_pressed():
+	pause();
+	$CanvasLayer/PopupPauseMenu.popup();
+	$CanvasLayer/pause_button.hide();
+
+
+func _on_button_resume_pressed():
+	resume();
+	$CanvasLayer/PopupPauseMenu.hide();
+	$CanvasLayer/pause_button.show();
+
+func _on_button_retry_pressed():
+	restart_scene();
+	resume();
