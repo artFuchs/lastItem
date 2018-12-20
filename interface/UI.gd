446d58e859e858c3872c201bd8087ad1ab2b1343
arthur fuchs <alfuchs17@gmail.com>
2018-12-20 20:06:45 -0200

    adicionado controles de audio
    jogador pode mutar musica ou FX

--- interface/UI.gd
+++ interface/UI.gd
@@ -1,7 +1,14 @@
 extends CanvasLayer
 
+onready var bm = $MarginContainer/split/buttons/music_button
+onready var bfx = $MarginContainer/split/buttons/fx_button
+func _ready():
+	bm.pressed = !AudioServer.is_bus_mute(AudioServer.get_bus_index("music"))
+	bfx.pressed = !AudioServer.is_bus_mute(AudioServer.get_bus_index("FX"))
+
+
 # in game UI interface
-onready var hud = $MarginContainer/HUD
+onready var hud = $MarginContainer/split/HUD
 onready var end = $EndLevelMenu
 func _on_player_changed_items(items):
 	hud.updateItems([]+items)
@@ -12,3 +19,9 @@ func show_win_message():
 func update_points(points):
 	hud.updatePoints(points)
 	end.updatePoints(points)
+	
+func set_music(on):
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("music"),!on)
+		
+func set_FX(on):
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("FX"),!on)
