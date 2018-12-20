e80c50418af5e5cd0e8afee9728ce0019b8eb4bf
gcwarken <gcwarken@gmail.com>
2018-12-20 18:19:01 -0200

    fixed dependencies that were causing crashes

--- objetos/laser/laserMovel.gd
+++ objetos/laser/laserMovel.gd
@@ -3,13 +3,14 @@ extends Path2D
 var tween
 var dir = false
 var changeDir = false
+export(int) var duration = 7
 
 func _ready():
 	set_process(true)
 	tween = Tween.new()
 	add_child(tween)
 
-	tween.interpolate_property($PathFollow2D, "unit_offset", 0, 1, 7, tween.TRANS_LINEAR, tween.EASE_IN_OUT)
+	tween.interpolate_property($PathFollow2D, "unit_offset", 0, 1, duration, tween.TRANS_LINEAR, tween.EASE_IN_OUT)
 	
 	tween.set_repeat(true)
 	tween.start()
