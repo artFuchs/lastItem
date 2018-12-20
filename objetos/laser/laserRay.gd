a0c7d3a849c4e56f33a061136bf00660b95b213d
gcwarken <gcwarken@gmail.com>
2018-12-20 17:48:40 -0200

    lvl 3 begins

--- objetos/laser/laserRay.gd
+++ objetos/laser/laserRay.gd
@@ -1,12 +1,17 @@
 extends StaticBody2D
 
+export(bool) var laserDefect = false
+var laserOn = true
+
+
 func _ready():
 	pass
 
 func _physics_process(delta):
-	if $RayCast2D.is_colliding():
-		var collider = $RayCast2D.get_collider()
-		if collider:
-			if collider.has_method("kill"):
-				collider.kill(self)
-		$Line2D.points[1].y = to_local($RayCast2D.get_collision_point()).y
+	if laserOn:
+		if $RayCast2D.is_colliding():
+			var collider = $RayCast2D.get_collider()
+			if collider:
+				if collider.has_method("kill"):
+					collider.kill(self)
+			$Line2D.points[1].y = to_local($RayCast2D.get_collision_point()).y
